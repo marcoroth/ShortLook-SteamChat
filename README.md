ec7ca03de9a30b5db0e887e0a1fb3119ef10d4b1
Marco Roth <marco.roth@intergga.ch>
2020-06-09 22:01:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# SteamChat Profile Pictures Provider for ShortLook
+
+Show SteamChat Profile Pictures in ShortLook when you receive a SteamChat notification!
+
+(This is not ShortLook itself, but a plugin for it)
---
+++ SteamChatProfilePictureProvider.m
@@ -0,0 +1,25 @@
+#import "SteamChatProfilePictureProvider.h"
+
+@implementation SteamChatProfilePictureProvider
+  - (DDNotificationContactPhotoPromiseOffer *)contactPhotoPromiseOfferForNotification:(DDUserNotification *)notification {
+    NSDictionary *payload = [notification applicationUserInfo];
+    NSString *steamId = [NSString stringWithFormat:@"%@", payload[@"steamid"]];
+    if (!steamId) return nil;
+
+    // Credit: JeffResc/ShortLook-Steam
+    // https://github.com/JeffResc/ShortLook-Steam/blob/64b0d016156085c3f9191c57f42e1874bd6a76c6/SteamContactPhotoProvider.m#L10
+    NSURL *profileURL = [NSURL URLWithString:[NSString stringWithFormat:@"https://steamcommunity.com/profiles/%@?xml=1",steamId]];
+    NSData *data = [NSData dataWithContentsOfURL: profileURL];
+    if (!data) return nil;
+
+    NSString *dataStr = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    NSString *imageURLStr = [[[[dataStr componentsSeparatedByString:@"<avatarFull><![CDATA["] objectAtIndex:1] componentsSeparatedByString:@"]]></avatarFull>"] objectAtIndex:0];
+
+    if (imageURLStr) {
+      NSURL *imageURL = [NSURL URLWithString:imageURLStr];
+      return [NSClassFromString(@"DDNotificationContactPhotoPromiseOffer") offerDownloadingPromiseWithPhotoIdentifier:imageURLStr fromURL:imageURL];
+    } else {
+      return nil;
+    }
+  }
+@end
